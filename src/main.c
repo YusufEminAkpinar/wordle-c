726c730b4130c8d654c7fd2279329cb17bb0a771
YusufEminAkpinar <yakpinar137@gmail.com>
2023-06-30 23:19:34 +0300

    Adding words list and cleaning up code. Still not working properly....

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "stdlib.h"
 #include "string.h"
 #include "stdbool.h"
+#include "time.h"
+#include "locale.h"
 
 #define max_char 5
 #define fifty_under "--------------------------------------------------"
@@ -14,8 +16,8 @@
 /*
 TODO:
 ========================
-DONE (I have officially solved my first memory overflow i'm so happy lol)
-1) Fix the fuckin buffer overflow... After 5 char next variable will be overwritten by previous input's indexs bigger than 5.
+DONE
+1) Fix the buffer overflow... After 5 char next variable will be overwritten by previous input's indexs bigger than 5.
 ie: if naughty user enters 12345guess as goalword, the guess has become "guess" without asking user input.
 Same thing happens if user enters aaaaabbbbb as guess then it will count it as 2 guess.
 
@@ -27,33 +29,58 @@ DONE
 
 4) Put some colors in it and give it a better look.
 
-5)Find a way for strings that contains same character twice.
+DONE
+5) Find a way for strings that contains same character twice.
+
+DONE
+6) Implement random chooser.
+
+DONE
+7) Seperate the known and wrong placed letters like previous commits and also print out the eliminated letters.
+
+8) Add a Turkish language support.
+
+9) I don't know why but guess can't be validated after third guess. Solve it.
 */
 
 /* Function Declarations */
 void toLowerCase(char string[]);
-char *removeDuplicate(char word[]);
-bool checkDupe(char word[]);
-bool isEqual(char word1[], char word2[]);
-void randomword(void);
+char *randomWord(void);
 char *chooseOwnWord(void);
-void introScreen(void);
-void mainGame(void);
+char *introScreen(void);
+void mainGame(char *goalWord);
 char *colorfulChar(char color[], char known_char[], int point);
 int *arr_of_correct_places(char word1[], char word2[]);
 bool isPresent(int num, int *arr, int arrSize);
 int num_of_occur(char c, char word[]);
+bool isValid(FILE *file, char *word);
 
 
 /* Variable Declarations */
 int chances = 6;
 
+
+#define gets
+
 int main(void) {
-    introScreen();
-	  mainGame();
-    return 0;
+	mainGame(introScreen());
+//	FILE *f = fopen("../words.txt", "r");
+//	bool c = isValid(f, "phone");
+    	return 0;
 }
 
+bool isValid(FILE *file, char *word){
+	char line[7];
+	if (file != NULL) {
+		while (fgets(line, sizeof(line), file) != NULL) {
+			//printf("line is %s", line);
+			if (!(strncmp(line, word, 5))) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
 
 int num_of_occur(char c, char word[]){
 	int k = 0;
@@ -79,26 +106,6 @@ int *arr_of_correct_places(char word1[], char word2[]){
 	return arr;
 }
 
-// why didn't i use strcmp()??
-bool isEqual(char word1[], char word2[]){
-	int sizew1 = strlen(word1);
-	int sizew2 = strlen(word2);
-	if (sizew1 > sizew2) /* making word2 a larger one. */
-	{
-		char tmp[strlen(word1)];
-		strcpy(tmp, word1); 
-		strcpy(word1, word2); 
-		strcpy(word2, tmp); 
-	}
-	
-	for (int i = 0; word2[i] != '\0'; i++)
-	{
-	if (word2[i] != word1[i])
-			return false;
-	}
-	return true;
-}
-
 void toLowerCase(char string[]){
 	for (int i = 0; string[i] != '\0'; ++i)
 	{
@@ -107,44 +114,6 @@ void toLowerCase(char string[]){
 		else 
 			string[i] = string[i];	
 	}
-}
-
-bool checkDupe(char word[]){
-	for (int i = 0; word[i] != '\0'; i++)
-	{
-		for (int j = i+1; word[j] != '\0'; j++)
-		{
-			if (word[i] == word[j])
-				return false;
-		}
-	}
-	return true;
-}
-
-char *removeDuplicate(char word[]){
-    char *new_word = malloc(sizeof(char) * strlen(word));
-	/* I do not know if there is better way to do this but it looks really *loopy*... */
-	strcpy(new_word, word);
-	while (!checkDupe(new_word)) /* Continue to run until no more duplicate */
-	{
-		for (int i = 0; i < new_word[i] != '\0'; i++) {
-			for (int j = i+1; new_word[j] != '\0'; j++)
-			{
-				if (new_word[i] == new_word[j])
-				{
-					for (int k = j; k <= (int)strlen(new_word); k++)
-					{
-						new_word[k] = new_word[k+1];
-					}
-				}		
-			}	
-		}
-	}
-    return new_word;
-}
-
-void randomWord(void){
-
 }
 
 
@@ -174,9 +143,33 @@ char *colorfulChar(char color[], char known_char[], int point){
 }
 
 
+char *randomWord(void){
+	char *word = malloc(sizeof(char) * 50);
+	srand(time(NULL));
+	int r = rand() % 5688;
+	int k = 0;
+	FILE *file = fopen("../words.txt", "r");
+	if (file != NULL) {
+		while (fgets(word, 50, file) != NULL){
+			if(k == r){
+				fclose(file);
+				return word;	
+			}
+			k++;
+		}
+	}
+	else {
+		fprintf(stderr, "%s\n", "File can not be opened.");
+	}
+	fclose(file);
+	return NULL;
+}
+
+
 char *chooseOwnWord(void){
 	char *goalWord = malloc(sizeof(char) * max_char);
 	char *temp = malloc(sizeof(char) * max_char);
+	FILE *file = fopen("../words.txt", "r");
     RESET:printf("Please enter your guess: \n");
 	scanf("%s", temp);
 	if (strlen(temp) != max_char)
@@ -184,30 +177,34 @@ char *chooseOwnWord(void){
 		printf("\nPlease enter 5 character world\n");
 		goto RESET;
 	}
+	if (!(isValid(file, temp))){
+		printf("Please enter a valid word.\n");
+		goto RESET;
+	}
 	system("clear");
 	printf("The word has been choosed by thy almighty user.\n");
 	strncpy(goalWord, temp, max_char);
 	free(temp);
+	fclose(file);
 	toLowerCase(goalWord);
 	return goalWord;
 }
 
-void introScreen(void){
+char *introScreen(void){
 	system("clear");
 	// printf(clear);
 	printf(" %s\n|\t\t\t\t\t\t   |\n|\t    Welcome to the Wordle game.\t\t   |\n|\t\t\t\t\t\t   |\n %s\n", fifty_under, fifty_under);
 	CHOOSE:printf("Please choose an option:\n  \033[34m1)\033[0m Choose a certain word\n  \033[34m2)\033[0m Let program choose a random word\n  \033[34m3)\033[0m Instructions\n\n  >> ");
 	int option;
+	char *goalWord;
 	scanf("%d", &option);
 	switch (option)
 	{
 	case 1: printf("Okay then choose a 5 letter word\n");
-		/* chooseOwnWord(); */
-		;
+		goalWord = chooseOwnWord();
 		break;
 	case 2: printf("Computer will choose a word for you\n");
-		/* randomWord(); */
-		;
+		goalWord = randomWord();
 		break;
 	case 3: printf("There is a secret word which contains exacly 5 characters. You have 6 chances for match the secret word. If you can match the exact spot for a character, it will turn to green. And it will turn to yellow if you can't. That's all about this game. Now please choose a option to start a game. \n\n\n");
 		goto CHOOSE;
@@ -215,7 +212,9 @@ void introScreen(void){
 	default: printf("Invalid option\n");
 		goto CHOOSE;
 	}
+	return goalWord;
 }
+
 
 bool isPresent(int num, int *arr, int arrSize){
 	for (int i = 1; i<arrSize; i++) {
@@ -227,11 +226,24 @@ bool isPresent(int num, int *arr, int arrSize){
 }
 
 
-void mainGame(void){
-	char *goalWord = chooseOwnWord();
+bool isPresentStr(char c, char *str){
+	for (size_t i = 0; i<strlen(str); i++) {
+		if (c == str[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+void mainGame(char *goalWord){
 	char *tmp = malloc(sizeof(char) * max_char);
-	char *guess_input = malloc(sizeof(char) * max_char);
+	char *guess = malloc(sizeof(char) * max_char);
+	FILE *file = fopen("../words.txt", "r");
 	char *last_state = malloc(sizeof(char) * max_char);
+	char *eliminated = malloc(sizeof(char) * 50);
+	int k = 0;
+	bool is_elim;
 	while (chances != 0){
 		for (int i = 0; i<max_char; i++) {
 			last_state[i] = '_';
@@ -243,31 +255,29 @@ void mainGame(void){
 			printf("\nPlease enter 5 character world\n");
 			goto RESET;
 		}
-		strncpy(guess_input, tmp, max_char);
+		strncpy(guess, tmp, max_char);
 		free(tmp);
-		toLowerCase(guess_input);
-	    /* char *guess = removeDuplicate(guess_input); */
-		char *guess = guess_input;
+		if (!(isValid(file, guess))) {
+			printf("The word %s, is not a valid word according to the dictionary. Please enter antoher one.\n", guess);
+			goto RESET;
+		}
 
-		/* printf("GoalWord is: %s\n", goalWord); */
-		if (isEqual(guess_input, goalWord))
-		{
+		if (!(strncmp(guess, goalWord, 5))) {
 			printf("Congratulations. You've won.\n");
 			break;
 		}
+
 		int *arr = arr_of_correct_places(guess, goalWord);
-		/* for (int i = 0; i<1+arr[0]; i++) { */
-		/* 	printf("Arr of corr place : %d\n", arr[i]); */
-		/* } */
 		for (int i = 0; i < max_char; ++i)
 		{
+			is_elim = true;
 			for (int j = 0; j < max_char; ++j)
 			{
 				if ((guess[i] == goalWord[j]) && (i==j))
-				/* if (isPresent(i, arr, arr[0])) */
 				{
 					/* printf("Letter %c is in correct place.\n", guess[i]); */
 					last_state[i] = (char)(guess[i] - 32);
+					is_elim = false;
 					/* guess = colorfulChar(green, guess, i); */
 				}
 				else if (guess[i] == goalWord[j] && !(isPresent(j, arr, arr[0])) &&
@@ -275,14 +285,22 @@ void mainGame(void){
 				{
 					/* printf("Letter %c is exist but in wrong place.\n", guess[i]); */
 					last_state[i] = guess[i];
+					is_elim = false;
 					/* guess = colorfulChar(pink, guess, i); */
 				}
 			}
+			if (is_elim && !(isPresentStr(guess[i], eliminated))) {
+				eliminated[k] = guess[i];
+				k++;
+				eliminated[k] = ' ';
+				k++;
+			}
 		}
 		chances--;
+		free(guess);
 		free(arr);
 		printf("\n\nThe last state of the word is:\n");
-		printf(" \t%s\t\t\t\tYou have %d chances left.\n", last_state, chances);
+		printf(" \t%s\t\t\t\tYou have %d chances left.\t\tEliminated keys are: %s\t\n", last_state, chances, eliminated);
 		free(last_state);
 		// printf("\n\n%s\n\n", chances, fifty_under);
 }
